739aabbf2a99b724a71f2d786dc1ae11a4951d7f
Kathia Amarante <seilamarante@hotmail.com>
2017-01-09 02:43:09 +0000

    Create the product logic
    Retrieve the product list
    Ad item to the shoppoing cart

---
+++ eCommerce/app/product/product.controller.js
@@ -0,0 +1,40 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('eCommerceApp')
+        .controller('productController', ProductController);
+
+    ProductController.$inject = ['productFactory'];
+
+    function ProductController(productFactory) {
+        var self = this;
+        self.products = [];
+
+        self.categories = [""];
+        getProductInventory();
+
+        function getProductInventory() {
+            productFactory.getProducts()
+                .then(function (data) {
+                    self.products = data;
+
+                    self.categories = setCategories(data);
+                });            
+        }
+
+        function setCategories(arr) {      
+
+            var allCategories = arr.map(function(item){
+                return item.category});
+
+            return  allCategories
+                .filter(function (item, pos, self) {
+                return self.indexOf(item) == pos;
+                })
+                .sort();
+        }
+
+    }
+
+})();
---
+++ eCommerce/app/product/product.factory.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('eCommerceApp')
+        .factory('productFactory',ProductFactory);
+
+    ProductFactory.$inject = ['$http'];
+
+    function ProductFactory($http) {
+        return {
+            getProducts: getProduct
+        }
+
+        function getProduct() {
+            return $http.get('app/product/data/product.json')
+                .then(getProductSucceeded, getProductFailed) ;
+
+            function getProductSucceeded(response) {
+                return  response.data.products;
+            }
+
+            function getProductFailed(error) {
+                //TODO: Implement the logger
+                console.log(error.data);
+            }
+        }
+       
+    }
+
+})();
